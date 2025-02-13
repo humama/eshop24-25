0671429ef0526026317441c5afd24d5d939c88b3
Humam Al Labib <humam.alabib@gmail.com>
2025-02-13 16:08:34 +0700

    commit ProductService.java and ProductServiceImpl.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,6 +4,6 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    Product createProduct(Product product);
+    Product create(Product product);
     List<Product> findAll();
 }
